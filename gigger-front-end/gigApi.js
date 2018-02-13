15d1c79affcccc31c43a2d7e72cb07fb8a888c03
julientregoat <julienctregoat@gmail.com>
2018-02-13 11:42:16 -0500

    finished up the parsing and fetching of gigs, next to render

--- gigger-front-end/gigApi.js
+++ gigger-front-end/gigApi.js
@@ -4,8 +4,16 @@ const GigApi = (function() {
       fetch("http://localhost:3000/tags").then(res => res.json()).then(json => console.log(json))
     }
 
-    static getGigs() {
-      fetch("http://localhost:3000/gigs").then(res => res.json()).then(json => console.log(json))
+    static fetchGigs() {
+      fetch("http://localhost:3000/gigs")
+      .then(res => res.json())
+      .then(json => {
+        for (const gig of json){
+          const newGig = new Gig({id: gig.id, title: gig.title, body: gig.body, poster_id: gig.poster.id, tag_id: gig.tag.id})
+          console.log(json)
+          console.log(newGig)
+        }
+      })
     }
   };
 })();
