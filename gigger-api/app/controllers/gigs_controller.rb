d7f8357de41073c93b3e5f045b5b64cdeab8d012
brianabaker <brianaclairbaker@gmail.com>
2018-02-13 09:59:13 -0500

    created user and gig controller

--- gigger-api/app/controllers/gigs_controller.rb
+++ gigger-api/app/controllers/gigs_controller.rb
@@ -1,2 +1,39 @@
 class GigsController < ApplicationController
+
+  before_action :set_gig, only: [:show,:update,:destroy]
+
+  def index
+    gigs = Gig.all
+    render json: gigs, status: 200
+  end
+
+  def create
+    gig = Gig.create(gig_params)
+    render json: gig, status: 201
+  end
+
+  def update
+    @gig.update(gig_params)
+    render json: @gig, status: 200
+  end
+
+  def destroy
+    gigId = @gig.id
+    @gig.destroy
+    render json: {message:"Zap! Gig deleted", gigId:gigId}
+  end
+
+  def show
+    render json: @gig, status: 200
+  end
+
+  private
+  def gig_params
+    params.permit(:title, :body, :poster_id, :tag_id)
+  end
+
+  def set_note
+    @gig = Gig.find(params[:id])
+  end
+
 end
