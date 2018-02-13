1ef5bcf7c741ba38179dabac0ada63ff823f4b01
julientregoat <julienctregoat@gmail.com>
2018-02-13 15:41:49 -0500

    merge?

--- gigger-front-end/gig.js
+++ gigger-front-end/gig.js
@@ -8,32 +8,6 @@ const Gig = (function() {
       this.tag_id = tag.id;
     }
 
-    static renderGigItem(gig){
-      let newGig = document.createElement('a')
-      newGig.className = "list-group-item list-group-item-action flex-column align-items-start"
-      newGig.href = "#"
-      newGig.innerHTML =
-      `<div class="d-flex w-100 justify-content-between">
-        <h5 class="mb-1">${gig.title}</h5>
-        <small>Date Posted</small>
-      </div>
-      <p class="mb-1">${gig.body}</p>
-      <span class="badge badge-success">${gig.tag}</span>`
-      //
-      // let newGigItem =
-      // `<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-      //   <div class="d-flex w-100 justify-content-between">
-      //     <h5 class="mb-1">${gig.title}</h5>
-      //     <small>Date Posted</small>
-      //   </div>
-      //   <p class="mb-1">${gig.body}</p>
-      //   <span class="badge badge-success">${gig.tag}</span>
-      // </a>`
-      console.log(newGig)
-      return newGig
-    }
-
-
     render() {
       let newGig = document.createElement('a')
       newGig.className = "list-group-item list-group-item-action flex-column align-items-start"
--- gigger-front-end/gigApi.js
+++ gigger-front-end/gigApi.js
@@ -1,17 +1,12 @@
 const GigApi = (function() {
   return class GigApi {
     static getTags(){
-      fetch("http://localhost:3000/tags").then(res => res.json()).then(json => console.log(json))
+      fetch("http://localhost:3000/tags").then(res => res.json()).then(json => json)
     }
 
     static fetchGigs() {
       return fetch("http://localhost:3000/gigs")
       .then(res => res.json())
-      // .then(json => {
-      //   for (const gig of json){
-      //     const newGig = new Gig(gig)
-      //   }
-      // })
     }
   };
 })();
