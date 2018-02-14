196e24357a21ba1745edf659410781584ce1161e
julientregoat <julienctregoat@gmail.com>
2018-02-14 11:30:11 -0500

    added comment validations, setting up submit new comment

--- gigger-api/app/models/comment.rb
+++ gigger-api/app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
 
   belongs_to :gig
   belongs_to :user
+
+  validates :gig_id, presence: true
+  validates :user_id, presence: true
+  validates :content, presence: true
 end
